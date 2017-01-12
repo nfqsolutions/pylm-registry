36888c9dd022b2a7e9f2f473a006de2fabae736d
Guillem Borrell <guillemborrell@gmail.com>
2017-01-12 12:44:41 +0100

    Tests for multiple requests

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -26,6 +26,21 @@ Processors = 3
 Ports_from = 5555
 """
 
+server1 = """
+[DEFAULT]
+Name = My Worker
+Ip = 127.0.0.1
+Processors = 1
+Ports_from = 5555
+"""
+server2 = """
+[DEFAULT]
+Name = My Worker
+Ip = 127.0.0.2
+Processors = 2
+Ports_from = 5555
+"""
+
 
 def test_00load_cluster():
     manager = ClusterManager(cluster)
@@ -45,3 +60,20 @@ def test_01simple_request():
                         '--db tcp://127.0.0.1:5559',
                         'python3 valuation_worker.py --db tcp://127.0.0.1:5559',
                         'python3 valuation_worker.py --db tcp://127.0.0.1:5559']
+
+
+def test_02multiple_requests():
+    manager = ClusterManager(cluster)
+
+    commands = manager.process_resource(server1)
+    assert commands == ['python3 valuation_standalone_master.py '
+                        '--pull tcp://127.0.0.1:5555 '
+                        '--pub tcp://127.0.0.1:5556 '
+                        '--workerpull tcp://127.0.0.1:5557 '
+                        '--workerpush tcp://127.0.0.1:5558 '
+                        '--db tcp://127.0.0.1:5559']
+
+    commands = manager.process_resource(server2)
+    assert commands == ['python3 valuation_worker.py --db tcp://127.0.0.1:5559',
+                        'python3 valuation_worker.py --db tcp://127.0.0.1:5559']
+
