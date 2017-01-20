86571cd9e10e1a6092603b0cb578be063e4bc5a1
Guillem Borrell <guillemborrell@gmail.com>
2017-01-20 11:43:39 +0100

    First untested implementation of the runner.

--- pylm/registry/runner.py
+++ pylm/registry/runner.py
@@ -1,8 +1,16 @@
 from pylm.registry.clients.registry import request
 from pylm.registry.clients import LogClient
 
-import subprocess
+from subprocess import Popen, PIPE
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 import argparse
+import configparser
+import traceback
+import zmq
+import sys
+
+context = zmq.Context()
 
 
 def parse_args():
@@ -14,6 +22,27 @@ def parse_args():
     return parser.parse_args()
 
 
+def message_hub(client):
+    pull_socket = context.socket(zmq.PULL)
+    pull_socket.bind("inproc://hub")
+
+    while True:
+        message = pull_socket.recv()
+        message = message.decode('utf-8')
+        message = message.strip('\n')
+        if message:
+            client.send(message)
+
+
+def process_wrapper(commands):
+    push_socket = context.socket(zmq.PUSH)
+    push_socket.connect("inproc://hub")
+
+    with Popen(commands, stdout=PIPE, bufsize=1, universal_newlines=True) as p:
+        for line in p.stdout:
+            push_socket.send(line.encode('utf-8'))
+
+
 def main():
     # If it does not provide all the args, it just dies.
     args = parse_args()
@@ -22,4 +51,25 @@ def main():
     with open(args.config) as config_data:
         commands = request(args.registry, args.cluster, config_data.read())
 
-    print(commands)
+    config = configparser.ConfigParser()
+    config.read(args.config)
+    processors = config.getint('DEFAULT', 'processors')
+    client = LogClient(args.registry, args.cluster)
+
+    futures = list()
+
+    with ThreadPoolExecutor(max_workers=processors+1) as executor:
+        for command in commands:
+            futures.append(executor.submit(process_wrapper, command.split()))
+
+        futures.append(executor.submit(message_hub, client))
+
+        # Only gets here in case of error
+        for future in as_completed(futures):
+            try:
+                print(future.result())
+            except:
+                print('{} generated an exception'.format(future))
+                lines = traceback.format_exception(*sys.exc_info())
+                for line in lines:
+                    print(line)
