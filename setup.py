1bee94177d3acdbced48f61ad9b4465fd7d7369d
Guillem Borrell <guillemborrell@gmail.com>
2017-01-30 14:31:38 +0100

    Updated documentation for the PYPI package and RTD build

--- setup.py
+++ setup.py
@@ -2,6 +2,40 @@
 
 from setuptools import setup
 
+long_description = """
+Pylm
+====
+
+Pylm is the Python implementation of PALM, a framework to build
+clusters of high performance components. Pylm-registry provides
+a set of tools to launch and monitor PALM clusters.
+
+**Pylm requires a version of Python equal or higher than 3.4, and it is
+more thoroughly tested with Python 3.5.**
+
+Installing **pylm** is as easy as:
+
+.. code-block:: bash
+
+   $> pip install pylm-registry
+
+* `PYPI package page <https://pypi.python.org/pypi/pylm-registry/>`_
+
+* `Documentation <http://pylm-registry.readthedocs.io/en/latest/>`_
+
+* `Source code <https://github.com/nfqsolutions/pylm-registry>`_
+
+Pylm is released under a dual licensing scheme. The source is released
+as-is under the the AGPL version 3 license, a copy of the license is
+included with the source. If this license does not suit you,
+you can purchase a commercial license from `NFQ Solutions
+<http://nfqsolutions.com>`_
+
+This project has been funded by the Spanish Ministry of Economy and
+Competitivity under the grant IDI-20150936, cofinanced with FEDER
+funds.
+"""
+
 setup(
     name='pylm-registry',
     description='Registry service to configure clusters of PALM components',
